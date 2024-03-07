84101e8e33dd3ac945b7b8dfb88516e42fe88d43
David Elliott <elliott.david.ballantyne@gmail.com>
2024-03-07 01:36:38 -0800

    Update watch.sh script

--- web/watch.sh
+++ web/watch.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-npx webpack --watch &
-while true; do
-    cp -r ./glsl ./dist/
-    cp -r ./stl ./dist/
-    cp -r ./img ./dist/
-    sudo cp -r ./dist/* /usr/share/nginx/html
-    inotifywait -e close_write ./dist/* ./glsl/* ./stl/*
-done
+npx vite --host
